21b8de8bacf71d2df2ebe3f8149f8199e6888748
Matt-Mcl <mattmcl2000@gmail.com>
2022-07-08 14:10:40 +0000

    Adds postgres integration

--- get_all.py
+++ get_all.py
@@ -1,5 +1,6 @@
 from helper import *
 from time import sleep
+import psycopg
 
 get_tables()
 urls = read_tables()
@@ -10,5 +11,13 @@ for puz_url, sol_url in urls:
     words = get_words(puz_url)
 
     print((puz_url, sol_url, words, puzzle_board, solution_board))
+
+    with psycopg.connect("dbname=gogen user=postgres") as conn:
+        with conn.cursor() as cur:
+            cur.execute(f"""
+                INSERT INTO puzzles(puzzle_url, solution_url, words, puzzle_board, solution_board)
+                VALUES(%s, %s, %s, %s, %s)
+                ON CONFLICT DO NOTHING;
+            """, (puz_url, sol_url, words, puzzle_board, solution_board))
 
     sleep(1)
--- get_today.py
+++ get_today.py
@@ -1,4 +1,5 @@
 from helper import *
+import psycopg
 
 get_tables()
 urls = read_tables()
@@ -9,3 +10,10 @@ words = get_words(urls[0][0])
 
 print((urls[0], words, puzzle_board, solution_board))
 
+with psycopg.connect("dbname=gogen user=postgres") as conn:
+    with conn.cursor() as cur:
+        cur.execute(f"""
+            INSERT INTO puzzles(puzzle_url, solution_url, words, puzzle_board, solution_board)
+            VALUES(%s, %s, %s, %s, %s)
+            ON CONFLICT DO NOTHING;
+        """, (urls[0][0], urls[0][1], words, puzzle_board, solution_board))

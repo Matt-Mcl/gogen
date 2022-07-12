d1d36ab4f5992adc19e5809cd62a5d4fc8c9f3c1
Matt-Mcl <mattmcl2000@gmail.com>
2022-07-12 15:41:24 +0000

    Adds statics and link to source of archive

--- gogensite/gogensite/settings.py
+++ gogensite/gogensite/settings.py
@@ -130,12 +130,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'

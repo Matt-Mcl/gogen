71abc546c37e22e11114d42387dd353434702358
Matt-Mcl <mattmcl2000@gmail.com>
2023-08-17 21:46:14 +0000

    Clears placeholders for completed puzzles

--- gogensite/gogen/apps.py
+++ gogensite/gogen/apps.py
@@ -7,15 +7,22 @@ class GogenConfig(AppConfig):
 
     def ready(self):
         from .models import PuzzleLog
-        puzzle_logs = PuzzleLog.objects.filter(status=PuzzleLog.STATUS_CHOICES[1][0])
+        incomplete_puzzle_logs = PuzzleLog.objects.filter(status=PuzzleLog.STATUS_CHOICES[1][0])
 
-        for pl in puzzle_logs:
+        for ipl in incomplete_puzzle_logs:
             # If any placeholders are filled, continue
-            if not all(all(element == '' for element in row) for row in pl.placeholders):
+            if not all(all(element == '' for element in row) for row in ipl.placeholders):
                 continue
             # If any letters have * next to them, continue
-            elif any(any(len(element) == 2 for element in row) for row in pl.board):
+            elif any(any(len(element) == 2 for element in row) for row in ipl.board):
                 continue
                 
             # If unedited, delete record to save space
-            pl.delete()
+            ipl.delete()
+
+        complete_puzzle_logs = PuzzleLog.objects.filter(status=PuzzleLog.STATUS_CHOICES[0][0])
+
+        for cpl in complete_puzzle_logs:
+            # clear all placeholders
+            cpl.placeholders = [['' for _ in range(5)] for _ in range(5)]
+            cpl.save()
---
+++ gogensite/start_dev_shell.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source "../venv/bin/activate"
+
+export DJANGO_SETTINGS_MODULE=gogensite.dev_settings
+
+python3 manage.py shell

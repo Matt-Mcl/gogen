3ea731bf093c75e3487308f0fb7bc36070f1defe
Matt-Mcl <mattmcl2000@gmail.com>
2023-08-22 13:36:23 +0000

    Adds basic selenium testing

--- gogensite/gogen/tests.py
+++ gogensite/gogen/tests.py
@@ -1,8 +1,30 @@
-from django.test import TestCase
-from django.contrib.auth.models import User
-from gogen.models import *
 import psycopg
+from gogen.models import *
+from datetime import datetime, timedelta
+from selenium import webdriver
 from django.conf import settings
+from django.test import TestCase, Client, RequestFactory
+from django.contrib.auth.models import AnonymousUser, User
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+def get_puzzle(puzzle_type, puzzle_date):
+
+    url = f"http://www.puzzles.grosse.is-a-geek.com/images/gog/puz/{puzzle_type}/{puzzle_type}{puzzle_date}puz.png"
+    
+    # Pull the puzzle from the database
+    with psycopg.connect(settings.PG_CONNECTION) as conn:
+        with conn.cursor() as cur:
+            cur.execute(f"SELECT * FROM {puzzle_type} WHERE puzzle_url = '{url}';")
+            puzzle = cur.fetchone()
+            if puzzle is None:
+                cur.execute(f"SELECT * FROM {puzzle_type} ORDER BY puzzle_name DESC LIMIT 1;")
+                puzzle = cur.fetchone()
+
+    return puzzle
 
 
 class PostgresTestCase(TestCase):
@@ -10,21 +32,14 @@ class PostgresTestCase(TestCase):
     test_puzzle_date = "20190120"
 
     def test_can_get_puzzle_from_db(self):
-        url = f"http://www.puzzles.grosse.is-a-geek.com/images/gog/puz/{self.test_puzzle_type}/{self.test_puzzle_type}{self.test_puzzle_date}puz.png"
+        puzzle = get_puzzle(self.test_puzzle_type, self.test_puzzle_date)
 
-        # Pull the puzzle from the database
-        with psycopg.connect(settings.PG_CONNECTION) as conn:
-            with conn.cursor() as cur:
-                cur.execute(f"SELECT * FROM {self.test_puzzle_type} WHERE puzzle_url = '{url}';")
-
-                puzzle = cur.fetchone()
-
-                name = puzzle[0]
-                puz_url = puzzle[1]
-                sol_url = puzzle[2]
-                words = puzzle[3]
-                board = puzzle[4]
-                solution_board = puzzle[5]
+        name = puzzle[0]
+        puz_url = puzzle[1]
+        sol_url = puzzle[2]
+        words = puzzle[3]
+        board = puzzle[4]
+        solution_board = puzzle[5]
         
         self.assertEqual(name, "uber20190120")
         self.assertEqual(puz_url, "http://www.puzzles.grosse.is-a-geek.com/images/gog/puz/uber/uber20190120puz.png")
@@ -68,3 +83,59 @@ class PuzzleLogTestCase(TestCase):
         self.assertEqual(user_puzzle_log[0].placeholders, [['', '', '', '', ''], ['', '', '', '', ''], ['', '', '', '', ''], ['', '', '', '', ''], ['', '', '', '', '']])
         self.assertEqual(user_puzzle_log[0].status, 'C')
         self.assertEqual(user_puzzle_log[0].notes, 'test notes with more')
+
+
+class DailyUberCase(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.factory = RequestFactory()
+        service = Service('/usr/bin/chromedriver')
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        cls.selenium = webdriver.Chrome(options=chrome_options, service=service)
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_can_load_daily_uber(self):
+        client = Client()
+        response = client.get('/')
+
+        self.assertEqual(response.status_code, 200)
+        
+    def test_can_unsucessfully_complete_daily_uber(self): 
+        self.selenium.get(f"{self.live_server_url}/")
+        letter_input = self.selenium.find_element(By.NAME, "01_letter")
+        letter_input.send_keys("Z")
+        submit_button = self.selenium.find_element(By.NAME, "submit_button")
+        submit_button.click()
+        status_heading = self.selenium.find_element(By.CLASS_NAME, "fadingHeading").text
+
+        self.assertEqual(status_heading, "Incorrect!")
+
+    def test_can_sucessfully_complete_daily_uber(self):
+        yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y%m%d')
+        puzzle = get_puzzle("uber", yesterday)
+
+        solution_board = puzzle[5]
+
+        self.selenium.get(f"{self.live_server_url}/")
+
+        letter_input = self.selenium.find_elements(By.CLASS_NAME, "form-control")
+
+        # Go through each input box and put in correct letter
+        for item in letter_input:
+            item.send_keys(solution_board[int(item.get_attribute('name')[0])][int(item.get_attribute('name')[1])])
+
+        submit_button = self.selenium.find_element(By.NAME, "submit_button")
+        submit_button.click()
+        status_heading = self.selenium.find_element(By.CLASS_NAME, "fadingHeading").text
+
+        self.assertEqual(status_heading, "Correct!")
